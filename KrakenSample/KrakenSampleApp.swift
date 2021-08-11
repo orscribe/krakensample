0c118f990b353b405278ba9414efdb5110e5e05d
Wesley Mitchell <wesley.Mitchell@coxautoinc.com>
2021-08-11 14:20:59 -0700

    Initial Commit

---
+++ KrakenSample/KrakenSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KrakenSampleApp.swift
+//  KrakenSample
+//
+//  Created by wesley Mitchell on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct KrakenSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
